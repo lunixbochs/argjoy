4b2ffb77809358b61520de34e2686b1136b4306c
Ryan Hileman <lunixbochs@gmail.com>
2015-12-27 08:37:14 -0800

    improve argjoy main test

--- argjoy_test.go
+++ argjoy_test.go
@@ -10,21 +10,12 @@ func test(a, b, optC int) int {
 }
 
 func TestArgjoy(t *testing.T) {
-	aj := NewArgjoy()
-	aj.Register(func(arg, val interface{}) (err error) {
-		if v, ok := val.(string); ok {
-			if a, ok := arg.(*int); ok {
-				*a, err = strconv.Atoi(v)
-				return
-			}
-		}
-		return NoMatchErr
-	})
+	aj := NewArgjoy(StrToInt)
 	aj.Optional = true
 
 	out, err := aj.Call(test, "1", "2")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if out[0].(int) != 3 {
 		t.Fatalf("Incorrect result: %v != 3\n", out[0])
