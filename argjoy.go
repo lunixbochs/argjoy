4352ffb51ae79ee5d4a992914e9080301961a303
Ryan Hileman <lunixbochs@gmail.com>
2015-12-27 11:34:51 -0800

    tweak the Convert interface

--- argjoy.go
+++ argjoy.go
@@ -49,23 +49,17 @@ func (a *Argjoy) translate(arg interface{}, vals []interface{}) error {
 	return nil
 }
 
-// Produce a list of arguments suitable for invoking reflect.Call(fn, args).
+// Given the arguments to a function as per reflect.In(),
+// produce a list of arguments suitable for invoking reflect.Call(fn, args).
 // If fn is variadic, you must instead use reflect.CallSlice(fn, args).
-func (a *Argjoy) Convert(fn interface{}, vals ...interface{}) ([]reflect.Value, error) {
-	fnv := reflect.ValueOf(fn)
-	if fnv.Kind() != reflect.Func {
-		panic(fmt.Sprintf("function expected as first argument, got: (%T)%#v", fn, fn))
-	}
-	fnt := fnv.Type()
-	argCount := fnt.NumIn()
-	in := make([]reflect.Value, argCount)
-	for i := 0; i < argCount; i++ {
-		argType := fnt.In(i)
+func (a *Argjoy) Convert(in []reflect.Type, variadic bool, vals ...interface{}) ([]reflect.Value, error) {
+	ret := make([]reflect.Value, len(in))
+	for i, argType := range in {
 		arg := reflect.New(argType)
-		if i == argCount-1 && fnt.IsVariadic() {
+		if i == len(in)-1 && variadic {
 			// this condition is nested because we don't want to ever fall to the next
 			// else block on variadic functions
-			if len(vals) >= argCount {
+			if len(vals) >= len(in) {
 				varargs := reflect.MakeSlice(argType, len(vals[i:]), len(vals[i:]))
 				arg.Elem().Set(varargs)
 				for j := range vals[i:] {
@@ -85,11 +79,11 @@ func (a *Argjoy) Convert(fn interface{}, vals ...interface{}) ([]reflect.Value, 
 			// so the rest of the args are zeroed (which happens automatically on reflect.New())
 		} else {
 			// we ran out of input and Optional arguments are disabled
-			return nil, &ArgCountErr{len(vals), argCount}
+			return nil, &ArgCountErr{len(vals), len(in)}
 		}
-		in[i] = arg.Elem()
+		ret[i] = arg.Elem()
 	}
-	return in, nil
+	return ret, nil
 }
 
 // Call fn(vals...), using registered codecs to convert argument types.
@@ -97,12 +91,19 @@ func (a *Argjoy) Convert(fn interface{}, vals ...interface{}) ([]reflect.Value, 
 // Will return an error value if any codec fails.
 // Panics if fn is not a valid function.
 func (a *Argjoy) Call(fn interface{}, vals ...interface{}) ([]interface{}, error) {
-	in, err := a.Convert(fn, vals...)
+	fnv := reflect.ValueOf(fn)
+	fnt := fnv.Type()
+	if fnv.Kind() != reflect.Func {
+		panic(fmt.Sprintf("function expected as first argument, got: (%T)%#v", fn, fn))
+	}
+	inTypes := make([]reflect.Type, fnt.NumIn())
+	for i := range inTypes {
+		inTypes[i] = fnt.In(i)
+	}
+	in, err := a.Convert(inTypes, fnt.IsVariadic(), vals...)
 	if err != nil {
 		return nil, err
 	}
-	fnv := reflect.ValueOf(fn)
-	fnt := fnv.Type()
 	var out []reflect.Value
 	if fnt.IsVariadic() {
 		out = fnv.CallSlice(in)
